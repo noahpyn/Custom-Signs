1b20276bb6a80cb2489053fd2d68037d95836790
Noah Payne(He/Him) <75100642+noahpyn@users.noreply.github.com>
2021-07-02 13:53:30 +0100

    Add files via upload

---
+++ Signs/main.js
@@ -0,0 +1,12 @@
+// using variables to store a number
+
+var price; 
+var quantity;
+var total;
+
+price = 5;
+quantity = 14;
+total = price * quantity ;
+
+var el = document.getElementById('cost');
+el.textContent = '£' + total;
